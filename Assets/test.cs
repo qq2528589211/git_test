03f981eeb9f85df70c1a968dbb87485ff7b0f509
hys <2528589211@qq.com>
2022-09-25 20:04:15 +0800

    git init

---
+++ Assets/test.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    private string name1="first";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
